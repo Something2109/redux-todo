c61d5eee707fcaacab74bc956ecdc12c84c374ca
HIEU Duong (VTI.D5) <hieu.duong@vti.com.vn>
2025-10-15 17:45:46 +0700

    feat(redux): create and configure redux store

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,32 @@
 import { BrowserRouter, Switch, Route } from "react-router-dom";
+import { Suspense } from "react";
+import { Provider as ReduxProvider } from "react-redux";
+import { PersistGate } from "redux-persist/integration/react";
+import { store, persistor } from "./redux/store";
 import Header from "./components/header";
 import routes from "./routes";
-import { Suspense } from "react";
 
 function App() {
   return (
-    <BrowserRouter>
-      <Header />
+    <PersistGate persistor={persistor} loading={<span>Loading...</span>}>
+      <ReduxProvider store={store}>
+        <BrowserRouter>
+          <Header />
 
-      <main className="h-screen dark:bg-gray-700 dark:text-white">
-        <Switch>
-          <Suspense fallback={<div>Loading...</div>}>
-            {routes.map(({ Component, ...route }, index) => (
-              <Route key={(route.path as string) ?? index} {...route}>
-                <Component />
-              </Route>
-            ))}
-          </Suspense>
-        </Switch>
-      </main>
-    </BrowserRouter>
+          <main className="h-screen dark:bg-gray-700 dark:text-white">
+            <Switch>
+              <Suspense fallback={<div>Loading...</div>}>
+                {routes.map(({ Component, ...route }, index) => (
+                  <Route key={(route.path as string) ?? index} {...route}>
+                    <Component />
+                  </Route>
+                ))}
+              </Suspense>
+            </Switch>
+          </main>
+        </BrowserRouter>
+      </ReduxProvider>
+    </PersistGate>
   );
 }
 
---
+++ src/redux/store.ts
@@ -0,0 +1,34 @@
+import {
+  applyMiddleware,
+  combineReducers,
+  legacy_createStore,
+  type Reducer,
+} from "redux";
+import { thunk, type ThunkAction } from "redux-thunk";
+import { persistStore, persistReducer } from "redux-persist";
+import autoMergeLevel2 from "redux-persist/es/stateReconciler/autoMergeLevel2";
+import storage from "redux-persist/lib/storage";
+
+const reducerObject = {};
+const mainReducer = combineReducers(reducerObject);
+
+type MainState = ReturnType<typeof mainReducer>;
+type ReducerAction = Parameters<typeof mainReducer>[1];
+type MainAction = ThunkAction<void, MainState, unknown, ReducerAction>;
+
+const persistedReducer = persistReducer<MainState, ReducerAction>(
+  {
+    key: "root",
+    storage,
+    whitelist: ["lang"],
+    stateReconciler: autoMergeLevel2,
+  },
+  mainReducer as unknown as Reducer<MainState, ReducerAction>
+);
+
+const store = legacy_createStore(persistedReducer, applyMiddleware(thunk));
+
+const persistor = persistStore(store);
+
+export { store, persistor };
+export type { MainState, ReducerAction, MainAction };
