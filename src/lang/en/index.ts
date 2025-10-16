7460f7159c104812de1134eaca03dfec0e0157e5
HIEU Duong (VTI.D5) <hieu.duong@vti.com.vn>
2025-10-16 09:14:45 +0700

    refactor(translation): add the common used translation text

--- src/lang/en/index.ts
+++ src/lang/en/index.ts
@@ -1,3 +1,4 @@
+import common from "./common";
 import home from "./home";
 
-export default { translation: { home } };
+export default { translation: { home, common } };
---
+++ src/lang/vi/common.ts
@@ -0,0 +1,4 @@
+export default {
+  title: "Công Việc Cần Làm",
+  loading: "Đang tải",
+};
