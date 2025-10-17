4b3c096591301ea04da3c082e22c0bf452620d82
HIEU Duong (VTI.D5) <hieu.duong@vti.com.vn>
2025-10-17 09:59:46 +0700

    feat(translation): create the error text

--- src/lang/en/common.ts
+++ src/lang/en/common.ts
@@ -3,6 +3,7 @@ export default {
   loading: "Loading",
   add: "Add",
   delete: "Delete",
+  error: "Encountered error when processing your request.",
   rule: {
     required: "The :attribute field is required.",
     min: "The :attribute must be at least :min:type.",
--- src/lang/vi/common.ts
+++ src/lang/vi/common.ts
@@ -3,6 +3,7 @@ export default {
   loading: "Đang tải",
   add: "Thêm",
   delete: "Xóa",
+  error: "Đã xảy ra lỗi khi xử lý yêu cầu của bạn.",
   rule: {
     required: "Trường :attribute là bắt buộc.",
     min: "Trường :attribute bắt buộc phải có ít nhất :min:type.",
